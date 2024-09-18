ac39c253bc16b26df57c514aef5ae7ae4c26f2d3
RussellLuo <luopeng.he@gmail.com>
2024-09-18 11:32:50 +0800

    add ava tests

---
+++ codenav-nodejs/__test__/index.spec.ts
@@ -0,0 +1,29 @@
+import test from 'ava'
+
+import path from 'path';
+const EXAMPLES_DIR = `${path.dirname(path.dirname(import.meta.dirname))}/examples`;
+
+import { Navigator, Snippet } from '../index';
+let nav = new Navigator('./test.sqlite');
+
+test('resloving references', (t) => {
+  nav.index([EXAMPLES_DIR], true);
+  
+  let snippet = new Snippet(`${EXAMPLES_DIR}/chef.py`, 2, 2);
+  
+  let defs: string[] = [];
+  for (let reference of snippet.references('')) {
+      let definitions = nav.resolve(reference);
+      let dependencies = definitions.filter(d => !snippet.contains(d));
+      if (dependencies.length == 0) continue;
+  
+      for (let [_, d] of dependencies.entries()) {
+          const path = d.path.slice(EXAMPLES_DIR.length);
+          defs.push(`${path}:${d.span.start.line}:${d.span.start.column}`);
+      }
+  }
+  
+  nav.clean(true);
+
+  t.deepEqual(defs, ['/chef.py:0:20', '/kitchen.py:2:4', '/stove.py:3:4'], 'bad definitions');
+})
